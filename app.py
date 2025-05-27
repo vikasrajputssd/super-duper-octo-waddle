45e2035c40cb50bf217762723e3221ba11d4fd0b
vikasrajputssd <vikashrajput9533@gmail.com>
2025-05-27 23:32:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="लाड़ली जू की कृपा ऐप", layout="centered")
+
+st.title("जय जय श्री राधे!")
+st.header("यह ऐप लाड़ली जू की सेवा में समर्पित है।")
+
+st.write("**राधावल्लभ श्री हरिवंश** नाम जप करते रहिए।")
+st.success("लाड़ली जू की कृपा से यह ऐप चल रहा है।")
